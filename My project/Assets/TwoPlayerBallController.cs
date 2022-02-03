fc727113fc5c1c87d815f47637d8842b4a930241
Troy <troy.stilting@st-2.com>
2022-02-03 14:27:24 +0100

    worked more on character movement

--- My project/Assets/TwoPlayerBallController.cs
+++ My project/Assets/TwoPlayerBallController.cs
@@ -5,19 +5,46 @@ using UnityEngine;
 public class TwoPlayerBallController : MonoBehaviour
 {
     //Hier maak je je eigen code voor character controller.
+  public Rigidbody rb;
+  public Rigidbody rb2;
+  public CharacterController movement;
+  public float speed;
+  public float gravity;
+  public Vector3 playerGravity;
+  public Vector3 moving;
+  public Vector3 moving2;
+  public int jumpHeight;
+  public bool onGround;
+  public ArrayList direction;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //Check voor grounded voor beide players.
+        moving = rb.position;
+        moving2 = rb2.position;
+        float horizontal1 = Input.GetAxis("Horizontal1");
+        float vertical1 = Input.GetAxis("Vertical1");
+        float horizontal2 = Input.GetAxis("Horizontal2");
+        float vertical2 = Input.GetAxis("Vertical2");  
+        rb.transform.position = new Vector3(horizontal1, 0, vertical1) * speed * Time.deltaTime;
+        rb2.transform.position = new Vector3(horizontal2, 0, vertical2) * speed * Time.deltaTime;
+        
+        //Check voor grounded voor beide players.(met raycast op floer of ontriggerstay)
+        if (onGround == true)
+        {
 
+        }
         //Check voor alle keys down en up en sla dit in een array op. 
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "")
+        {
+            onGround = true;
+        }
+        else
+        {
+            onGround = false;
+        }
     }
 }
