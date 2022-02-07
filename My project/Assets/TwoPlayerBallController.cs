0cf9d0f932fa92d469340b20d33c9c5305f684cf
Troy <troy.stilting@st-2.com>
2022-02-07 14:48:47 +0100

    gravity works now

--- My project/Assets/TwoPlayerBallController.cs
+++ My project/Assets/TwoPlayerBallController.cs
@@ -4,47 +4,63 @@ using UnityEngine;
 
 public class TwoPlayerBallController : MonoBehaviour
 {
-    //Hier maak je je eigen code voor character controller.
-  public Rigidbody rb;
-  public Rigidbody rb2;
-  public CharacterController movement;
-  public float speed;
-  public float gravity;
-  public Vector3 playerGravity;
-  public Vector3 moving;
-  public Vector3 moving2;
-  public int jumpHeight;
-  public bool onGround;
-  public ArrayList direction;
+    public Rigidbody rb;
+    public Rigidbody rb2;
+    public float speed;
+    public float speed2;
+    public float gravity;
+    public float gravity2;
+    public Vector3 moving;
+    public Vector3 moving2;
+    public int jumpHeight;
+    public int jumpHeight2;
+    public bool onGround;
+    public bool onGround2;
 
-    void Update()
+    public float versnelling;
+
+    void FixedUpdate()
     {
         moving = rb.position;
         moving2 = rb2.position;
         float horizontal1 = Input.GetAxis("Horizontal1");
         float vertical1 = Input.GetAxis("Vertical1");
         float horizontal2 = Input.GetAxis("Horizontal2");
-        float vertical2 = Input.GetAxis("Vertical2");  
-        rb.transform.position = new Vector3(horizontal1, 0, vertical1) * speed * Time.deltaTime;
-        rb2.transform.position = new Vector3(horizontal2, 0, vertical2) * speed * Time.deltaTime;
-        
+        float vertical2 = Input.GetAxis("Vertical2");
+        rb.velocity = new Vector3 (horizontal1, gravity, vertical1) * speed;
+        rb2.velocity = new Vector3 (horizontal2, gravity2, vertical2) * speed;
+
         //Check voor grounded voor beide players.(met raycast op floer of ontriggerstay)
+        if (Input.GetKeyDown (KeyCode.RightShift) && onGround == true)
+        {
+            rb.AddForce (0, 5, 0);
+        }
+        
+        if (Input.GetKeyDown (KeyCode.Space))
+        {
+            rb2.AddForce(Vector3.up * 100, ForceMode.Impulse);
+        }
+
+
+
+        
         if (onGround == true)
         {
-
-        }
-        //Check voor alle keys down en up en sla dit in een array op. 
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.name == "")
-        {
-            onGround = true;
+            
+            gravity = 0;
         }
         else
         {
-            onGround = false;
+            gravity += versnelling * Time.deltaTime;
+        }
+
+        if (onGround2 == true)
+        {
+            gravity2 = 0;
+        }
+        else
+        {
+            gravity2 += versnelling * Time.deltaTime;
         }
     }
 }
