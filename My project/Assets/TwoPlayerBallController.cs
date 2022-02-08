aea811c13824966d15f665905d4b0e79c51fcf39
Troy <troy.stilting@st-2.com>
2022-02-08 11:01:07 +0100

    gravity works... for real now

--- My project/Assets/TwoPlayerBallController.cs
+++ My project/Assets/TwoPlayerBallController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class TwoPlayerBallController : MonoBehaviour
 {
+    public GameManager gamemanager;
     public Rigidbody rb;
     public Rigidbody rb2;
+    public GameObject player;
+    public GameObject player2;
     public float speed;
     public float speed2;
     public float gravity;
@@ -16,8 +19,8 @@ public class TwoPlayerBallController : MonoBehaviour
     public int jumpHeight2;
     public bool onGround;
     public bool onGround2;
-
     public float versnelling;
+    public float versnelling2;
 
     void FixedUpdate()
     {
@@ -27,40 +30,41 @@ public class TwoPlayerBallController : MonoBehaviour
         float vertical1 = Input.GetAxis("Vertical1");
         float horizontal2 = Input.GetAxis("Horizontal2");
         float vertical2 = Input.GetAxis("Vertical2");
-        rb.velocity = new Vector3 (horizontal1, gravity, vertical1) * speed;
-        rb2.velocity = new Vector3 (horizontal2, gravity2, vertical2) * speed;
+        rb.velocity = new Vector3 (horizontal1, 0, vertical1) * speed;
+        rb2.velocity = new Vector3 (horizontal2, 0, vertical2) * speed2;
 
-        //Check voor grounded voor beide players.(met raycast op floer of ontriggerstay)
+
         if (Input.GetKeyDown (KeyCode.RightShift) && onGround == true)
         {
-            rb.AddForce (0, 5, 0);
+            Jump(player, rb);
         }
-        
+
         if (Input.GetKeyDown (KeyCode.Space))
         {
-            rb2.AddForce(Vector3.up * 100, ForceMode.Impulse);
+            Jump(player2, rb2);
         }
-
-
-
         
-        if (onGround == true)
-        {
-            
-            gravity = 0;
-        }
-        else
-        {
-            gravity += versnelling * Time.deltaTime;
-        }
+        // if (onGround == true || gamemanager.active == false)
+        // {    
+        //     gravity = 0;
+        // }
+        // else
+        // {
+        //     gravity += versnelling * Time.deltaTime;
+        // }
 
-        if (onGround2 == true)
-        {
-            gravity2 = 0;
-        }
-        else
-        {
-            gravity2 += versnelling * Time.deltaTime;
-        }
+        // if (onGround2 == true || gamemanager.active == false)
+        // {
+        //     gravity2 = 0;
+        // }
+        // else
+        // {
+        //     gravity2 += versnelling2 * Time.deltaTime;
+        // }
+    }
+
+    public void Jump(GameObject playerNumber, Rigidbody rbNumber)
+    {
+        rbNumber.AddForce(Vector3.up * jumpHeight2, ForceMode.Impulse);
     }
 }
