5ea37baa3c8d3fbbd82db584e2d330d45605193d
Troy <troy.stilting@st-2.com>
2022-02-15 14:28:59 +0100

    tag works now

--- My project/Assets/Scripts/CheckForGround.cs
+++ My project/Assets/Scripts/CheckForGround.cs
@@ -8,24 +8,30 @@ public class CheckForGround : MonoBehaviour
     public bool  option1 = false;
     private void OnTriggerEnter(Collider other)
     {
-        if(option1)
+        if(other.transform.tag == "Ground")
         {
-            twoPlayerBallController.onGround = true;
-        }
-        else
-        {
-            twoPlayerBallController.onGround2 = true;
+            if(option1)
+            {
+                twoPlayerBallController.onGround = true;
+            }
+            else
+            {
+                twoPlayerBallController.onGround2 = true;
+            }
         }
     }
-        private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other)
     {
-        if(option1)
+        if(other.transform.tag == "Ground")
         {
-            twoPlayerBallController.onGround = false;
-        }
-        else
-        {
-            twoPlayerBallController.onGround2 = false;
+            if(option1)
+            {
+                twoPlayerBallController.onGround = false;
+            }
+            else
+            {
+                twoPlayerBallController.onGround2 = false;
+            }
         }
     }
 }
